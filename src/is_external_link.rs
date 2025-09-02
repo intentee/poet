ed71bb474ab215747ebe642256a19e04a09c43c0
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2025-09-02 20:01:47 +0200

    handle mailto links

--- src/is_external_link.rs
+++ src/is_external_link.rs
@@ -1,3 +1,3 @@
 pub fn is_external_link(link: &str) -> bool {
-    link.starts_with("http:") || link.starts_with("https://")
+    link.starts_with("http:") || link.starts_with("https:") || link.starts_with("mailto:")
 }

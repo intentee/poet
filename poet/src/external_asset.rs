9c18d298b2d048efe58231f937f50419f62b417d
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2026-03-04 01:09:30 +0100

    escape HTML attributes in external asset URLs, handle protocol-relative URLs

--- poet/src/external_asset.rs
+++ poet/src/external_asset.rs
@@ -1,4 +1,5 @@
 use esbuild_metafile::renders_path::RendersPath;
+use rhai_components::escape_html_attribute::escape_html_attribute;
 
 #[derive(Clone, Eq, Ord, PartialEq, PartialOrd)]
 pub enum ExternalAsset {
@@ -10,16 +11,14 @@ impl ExternalAsset {
     pub fn render<TRendersPath: RendersPath>(&self, renders_path: &TRendersPath) -> String {
         match self {
             ExternalAsset::Script(url) => {
-                format!(
-                    "<script src=\"{}\" async defer></script>",
-                    renders_path.render_path(url),
-                )
+                let escaped_url = escape_html_attribute(&renders_path.render_path(url));
+
+                format!("<script src=\"{escaped_url}\" async defer></script>")
             }
             ExternalAsset::Stylesheet(url) => {
-                format!(
-                    "<link rel=\"stylesheet\" href=\"{}\">",
-                    renders_path.render_path(url),
-                )
+                let escaped_url = escape_html_attribute(&renders_path.render_path(url));
+
+                format!("<link rel=\"stylesheet\" href=\"{escaped_url}\">")
             }
         }
     }
@@ -30,11 +29,15 @@ mod tests {
     use super::*;
     use crate::asset_path_renderer::AssetPathRenderer;
 
+    fn create_path_renderer() -> AssetPathRenderer {
+        AssetPathRenderer {
+            base_path: "/".to_string(),
+        }
+    }
+
     #[test]
     fn script_renders_async_defer_tag() {
-        let path_renderer = AssetPathRenderer {
-            base_path: "/".to_string(),
-        };
+        let path_renderer = create_path_renderer();
 
         let asset = ExternalAsset::Script(
             "https://challenges.cloudflare.com/turnstile/v0/api.js".to_string(),
@@ -47,10 +50,21 @@ mod tests {
     }
 
     #[test]
+    fn script_escapes_url_in_attribute() {
+        let path_renderer = create_path_renderer();
+
+        let asset =
+            ExternalAsset::Script("https://example.com/script.js?foo=1&bar=\"test\"".to_string());
+
+        assert_eq!(
+            asset.render(&path_renderer),
+            "<script src=\"https://example.com/script.js?foo=1&bar=&quot;test&quot;\" async defer></script>",
+        );
+    }
+
+    #[test]
     fn stylesheet_renders_link_tag() {
-        let path_renderer = AssetPathRenderer {
-            base_path: "/".to_string(),
-        };
+        let path_renderer = create_path_renderer();
 
         let asset =
             ExternalAsset::Stylesheet("https://fonts.googleapis.com/css2?family=Inter".to_string());
